901d439d1f2b1e14121c7efcfacdd9cabe802cb9
Ignacio Calderón <53590021+Apollo13-0@users.noreply.github.com>
2023-11-16 22:37:42 -0600

    contador con sw

--- contadorDireccion.sv
+++ contadorDireccion.sv
@@ -1,16 +1,24 @@
-// TODO: hacer este contador con un input btn que seleccione si contar en el rango 0-9999 o 10000-19999
-
 module contadorDireccion (
-	 input [2:0] btn,
+    input wire switch,
     input wire clk,
     output reg [13:0] count
 );
 
 always @(posedge clk) begin
-    if (count == 14'd9999) begin
-        count <= 14'd0;
+    if (switch == 1'b0) begin
+        // Contar de 0 a 9999
+        if (count == 14'd9999) begin
+            count <= 14'd0;
+        end else begin
+            count <= count + 1;
+        end
     end else begin
-        count <= count + 1;
+        // Contar de 10000 a 19999
+        if (count == 14'd19999) begin
+            count <= 14'd10000;
+        end else begin
+            count <= count + 1;
+        end
     end
 end
 
