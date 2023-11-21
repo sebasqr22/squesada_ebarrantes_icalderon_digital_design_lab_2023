15d5a78c31d3b318ebd28e73d467e9836e2f2e8c
Erick Barrantes <erick.cr12@outlook.com>
2023-11-21 14:14:47 -0600

    Modificacion filtro_neg

--- filtro_neg.s
+++ filtro_neg.s
@@ -1,20 +1,8 @@
-.text
-.global _start
-_start:
-    mov r0, #0 @ Load the address of IMAG into r0
-	mov r1, #100
-	mul r1, r1, r1
-    mov r2, #100 @ Load the value of SIZE into r1
-
-loop_imag_ini:
-    mov r3, #0  @ contador de loop_imag (loop counter)
-    mul r4, r2, r2 @valor máximo de loop_imag (SIZE * SIZE)
-
 loop_imag_body:
-	ldr r5, [r0]  @ Carga valor actual de matriz, aumenta la direccion
+
+	ldr r5, [r0]
 	add r0, #4
-	mov r6, #255
-	sub r5, r6, r5   @ pixel = 255 - pixel
+	sub r5, r6, r5
 
 	str r5, [r1]
 	add r1, #4
